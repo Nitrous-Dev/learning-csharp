ce802faad44ca36091e29a322fe6219fdc0ae9eb
speedy <65173830+speedyfilms@users.noreply.github.com>
2022-03-21 08:31:08 -0400

    hi

--- program.cs
+++ program.cs
@@ -6,14 +6,24 @@ namespace HelloWorld
   {
     static void Main(string[] args)
     {
+      string myName = "Rohan";
+      int favoriteNumber = 53;
+      char theLetter;
+      theLetter = 'R';
+
       // This is my first comment.
       /* This is my first
       multi line comment.*/
+
       Console.WriteLine("Hello World!");
       Console.WriteLine("Hello World, but on a different line.");
 
       Console.Write("This is a sentence.");
-      Console.Write("This is another sentence.");
+      Console.WriteLine("This is another sentence.");
+
+      Console.WriteLine("Hello, my name is " + myName + ".");
+      Console.WriteLine(favoriteNumber + " is my favorite number.");
+      Console.WriteLine("The first letter of my name is " + theLetter + ".");
     }
   }
 }
