a29b8ce974f7bb581aaa8d5a2cdf7ade7dcc31c0
speedy <65173830+speedyfilms@users.noreply.github.com>
2022-03-22 20:28:23 -0400

    Az

--- testing-area.cs
+++ testing-area.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace HelloWorld
+namespace TestingArea
 {
   class Program
   {
---
+++ user-input.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please enter your favorite video game console:")
+
+        string favoriteConsole = Console.ReadLine();
+
+        Console.WriteLine("Your favorite video game console is the: " + favoriteConsole)
+
+    }
+  }
+}
