f89d6325949a99cac55de646527d7521660067c1
speedy <65173830+speedyfilms@users.noreply.github.com>
2022-03-23 17:19:34 -0400

    More User Input

--- user-input.cs
+++ user-input.cs
@@ -6,12 +6,23 @@ namespace HelloWorld
   {
     static void Main(string[] args)
     {
-        Console.WriteLine("Please enter your favorite video game console:")
+        Console.WriteLine("Please enter your favorite video game console:");
 
         string favoriteConsole = Console.ReadLine();
 
-        Console.WriteLine("Your favorite video game console is the: " + favoriteConsole)
+        Console.WriteLine("Your favorite video game console is the: " + favoriteConsole);
 
+        Console.WriteLine("Please enter your favorite number:");
+
+        int favoriteNumber = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Your favorite number is: " + favoriteNumber);
+
+        Console.WriteLine("Please enter your favorite video game engine:");
+
+        string favoriteEngine = Console.ReadLine();
+
+        Console.WriteLine("Your favorite video game engine is: " + favoriteEngine);
     }
   }
 }
